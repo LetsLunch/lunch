abab108a6b776002d7a4b7d49a1c297ff70d6c11
Rajat Khanna <rajatkhanna82@gmail.com>
2014-05-27 12:40:16 -0700

    Added neo4j architect

--- server/main/config.js
+++ server/main/config.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var Architect = require('neo4j-architect');
+NEO4J_URL = "http://localhost:"+7474 ;
 
+
+Architect.init();
+
+v
 module.exports = function (app, server) {
   app.set('port', process.env.PORT || 8008);
 
