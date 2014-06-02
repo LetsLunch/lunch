fa2b30b9745e1a7c2314c599c5a3b7d78fe8f09d
zzmp <zmp@umich.edu>
2014-06-02 01:52:13 -0700

    Added pushes collections for api

---
+++ server/main/models/pushes.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// ## Module Dependencies
+var _ = require('lodash');
+var Push = require('./neo4j/push.js');
+var Architect = require('neo4j-architect');
+
+Architect.init();
+
+var Construct = Architect.Construct;
+var Cypher = Architect.Cypher;
+
+// ## Results Functions
+// To be combined with queries using _.partial()
+
+// return a single tag
+var _singleTag = function (results, callback) {
+  if (results.length) {
+    callback(null, new Push(results[0].tag));
+  } else {
+    callback(null, null);
+  }
+};
+
+// creates the user with cypher
+var _create = function (params, callback) {
+  console.log('params :->',params);
+  var cypherParams = {
+    id : params.id,
+    token : params.token,
+    type : params.type,
+  };
+
+  var query = [
+    'MATCH (user:User{id:{userId}})',
+    'WITH user',
+    'MERGE (push:Push{token:{token},type:{type}})',
+    'CREATE UNIQUE (user)<-[:PUSH_TO]-(push)',
+    'RETURN push',
+  ].join('\n');
+
+  console.log('create query', query);
+
+  callback(null, query, cypherParams);
+};
+
+// delete the user and any relationships with cypher
+var _delete = function (params, callback) {
+  var cypherParams = {
+    token: params.id
+  };
+
+  var query = [
+    'MATCH (push:Push {token:{token}})',
+    'OPTIONAL MATCH (push)-[r:PUSH_TO]->()',
+    'DELETE push, r',
+  ].join('\n');
+
+  callback(null, query, cypherParams);
+};
+
+var create = new Construct(_create, _singleTag);
+var deletePush = new Construct(_delete);
+
+// export exposed functions
+module.exports = {
+  create: create.done(),
+  deletePush: deletePush.done()
+};
---
+++ server/main/routes/api/pushes.js
@@ -0,0 +1,125 @@
+'use strict';
+
+// ## Module Dependencies
+var _ = require('lodash');
+var sw = require('swagger-node-express');
+var utils = require('../../utils');
+
+// ## Models
+var Pushes = require('../../models/pushes');
+
+var param = sw.params;
+var swe = sw.errors;
+
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase(
+    process.env['NEO4J_URL'] ||
+    'http://localhost:7474'
+);
+
+
+// ## Helpers
+var _prepareParams = function (req) {
+  var params = req.body;
+
+  params.id = req.params.id || req.body.id;
+
+  return params;
+};
+
+// callback helper function
+// 
+// This is meant to be bound to a new function within the endpoint request callback
+// using _partial(). The first two parameters should be provided by the request callback 
+// for the endpoint this is being used in.
+//
+// Example:
+//
+// action: function(req, res) {
+//   var errLabel = 'Route: POST /tags';
+//   var callback = _.partial(_callback, res, errLabel);
+// }
+var _callback = function (res, errLabel, err, results, queries) {
+  var start = new Date();
+
+  if (err || !results) {
+    if (err) console.error(errLabel + ' ', err);
+    swe.invalid('input', res);
+    return;
+  }
+
+  utils.writeResponse(res, results, queries, start);
+};
+
+
+// ## API Specs
+
+// Route: POST '/tags'
+exports.addPush = {
+  
+  spec: {
+    path : '/pushes',
+    notes : 'Adds a push notification token to the graph',
+    summary : 'Add a new push notification token to the graph',
+    method: 'POST',
+    type : 'object',
+    items : {
+      $ref: 'Push'
+    },
+    parameters : [
+      param.form('userId', 'User UUID', 'string', true),
+      param.form('token', 'Push notification token', 'string', true),
+      param.form('type', 'Service type (gcm/apn)', 'string', true),
+     ],
+    responseMessages : [swe.invalid('input')],
+    nickname : 'addPush'
+  },
+
+  action: function(req, res) {
+    var options = {};
+    var params = {};
+    var errLabel = 'Route: POST /pushes';
+    var callback = _.partial(_callback, res, errLabel);
+
+    options.neo4j = utils.existsInQuery(req, 'neo4j');
+    params = _prepareParams(req);
+    // Check for params 
+    console.log('addPush params:-' + params);
+    Pushes.create(params, options, callback);
+
+  }
+};
+
+// // Route: DELETE '/tags/:id'
+exports.deleteTagRelation = {
+
+  spec: {
+    path: '/tags/{token}',
+    notes: 'Deletes an existing push notification token and its relationship',
+    summary: 'Delete a push notification token and its relationship',
+    method: 'DELETE',
+    type: 'object',
+    parameters: [
+      param.path('token', 'Push notification token to be deleted', 'string', true)
+
+    ],
+    responseMessages: [swe.invalid('input')],
+    nickname : 'deleteTagRelation'
+  },
+
+  action: function (req, res) {
+    var token = req.params.token;
+    var options = {};
+    var params = {};
+
+    if (!token) throw swe.invalid('token');
+
+    var errLabel = 'Route: DELETE /tags/{id}';
+    var callback = _.partial(_callback, res, errLabel);
+
+    options.neo4j = utils.existsInQuery(req, 'neo4j');
+    params = _prepareParams(req);
+
+    Pushes.deleteTagRelation(params, options, callback);
+  }
+};
