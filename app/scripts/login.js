58394c76322b163aa4744f00437b0685bf681855
zzmp <zmp@umich.edu>
2014-06-04 23:52:21 -0700

    Allow un/re-reg with GCM and browser mock polyfill

--- app/scripts/login.js
+++ app/scripts/login.js
@@ -15,10 +15,11 @@ angular.module('Lunch.login', ['Lunch.logout'])
   });
 })
 
-.controller('LoginCtrl', function($scope, $state, $ionicSlideBoxDelegate, OpenFB) {
+.controller('LoginCtrl', function($scope, $state, $ionicSlideBoxDelegate, OpenFB, push) {
   $scope.login = function() {
     OpenFB.login('public_profile,user_likes').then(
       function() {
+        push.register();
         $state.go('app.profile');
       },
       function() {
