73778db2d911bd31b54f355ccad9b17302b23691
Rajat Khanna <rajatkhanna82@gmail.com>
2014-06-04 20:15:36 -0700

    Removed extra logs

--- server/main/models/match.js
+++ server/main/models/match.js
@@ -4,7 +4,6 @@
 var _ = require('lodash');
 var User = require('./neo4j/user.js');
 var UserProfile = require('./neo4j/userprofile.js');
-var zipcoder = require('cities');
 var Architect = require('neo4j-architect');
 var colog = require('colog');
 
@@ -34,22 +33,12 @@ var _singleUserProfile = function (results, callback) {
 };
 
 
-// return many users
-var _manyUsers = function (results, callback) {
-  var users = _.map(results, function (result) {
-    return new User(result.results);
-  });
-
-  callback(null, users);
-};
-
 
 // ## Query Functions
 // Should be combined with results functions using _.partial()
 
 // find location of UserId with cypher
 var _findAllMatches = function (params, callback) {
-  colog.info(params);
   var date =  new Date(Date.now());
   var cypherParams = {
     userId : params.id,
@@ -57,17 +46,17 @@ var _findAllMatches = function (params, callback) {
   };
 
   var query = [
-  'MERGE (today:Date{id:{date}})',
-  'WITH today',
-  'MATCH (u:User{id:{userId}})-[:IS_AT]->(loc:Location)<-[:IS_AT]-(o:User)',
-  'WITH u, o,today',
-  'MATCH (u:User)-[r1:LIKES|HAS_TAG]->(l)<-[r2:LIKES|HAS_TAG]-(o:User)',
-  'WHERE NOT (u)-[:REJECT]-(o) AND NOT (u)-[:ACCEPT]->(o) AND NOT (o)-[:PAIRED_FOR]-(today)',
-  'WITH COUNT(DISTINCT l) AS cntlikes,o',
-  'WITH o ORDER BY cntlikes DESC',
-  'WITH HEAD(collect(o)) as user',
-  'MATCH (user)-[:LIKES]->(like),(user)-[:HAS_TAG]->(tag),(user)-[:IS_AT]->(location)',
-  'RETURN user,collect(DISTINCT like) as likes ,collect(DISTINCT tag) as tags,location'
+    'MERGE (today:Date{id:{date}})',
+    'WITH today',
+    'MATCH (u:User{id:{userId}})-[:IS_AT]->(loc:Location)<-[:IS_AT]-(o:User)',
+    'WITH u, o, today',
+    'MATCH (u:User)-[r1:LIKES|HAS_TAG]->(l)<-[r2:LIKES|HAS_TAG]-(o:User)',
+    'WHERE NOT (u)-[:REJECT]-(o) AND NOT (u)-[:ACCEPT]->(o) AND NOT (o)-[:PAIRED_FOR]-(today)',
+    'WITH COUNT(DISTINCT l) AS cntlikes,o',
+    'WITH o ORDER BY cntlikes DESC',
+    'WITH HEAD(collect(o)) as user',
+    'MATCH (user)-[:LIKES]->(like),(user)-[:HAS_TAG]->(tag),(user)-[:IS_AT]->(location)',
+    'RETURN user, collect(DISTINCT like) as likes, collect(DISTINCT tag) as tags, location'
   ].join('\n');
 
   colog.log('create query', query);
@@ -83,7 +72,6 @@ var _selected = function (params, callback) {
     date : date.toDateString()
   };
   var query; 
-  console.log(params.accepted);
   if(params.accepted === 'true'){
 
     query = [
@@ -92,14 +80,14 @@ var _selected = function (params, callback) {
       'MATCH (u:User{id:{userId}}),(o:User{id:{selectedUserId}}),(today)',
       'WHERE NOT (u)-[:REJECT]-(o) OR NOT (o)-[:PAIRED_FOR]-(today)',
       'CREATE UNIQUE (u)-[:ACCEPT]->(o)',
-      'WITH u,o,today',
+      'WITH u, o, today',
       'MATCH (o)-[:ACCEPT]->(u)',
       'CREATE UNIQUE (o)-[:PAIRED_FOR]->(today)<-[:PAIRED_FOR]-(u)',
       'RETURN o as user'
     ].join('\n');
     
-  }else{
-  query = [
+  }else if(params.accepted === 'false'){
+    query = [
       'MATCH (u:User{id:{userId}}),(o:User{id:{selectedUserId}})',
       'CREATE UNIQUE (u)-[r:REJECT]-(o)',
       'RETURN o as user'
