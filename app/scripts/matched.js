e887dcf84073698345ef86defa5739e0a8827174
zzmp <zmp@umich.edu>
2014-06-06 00:43:47 -0700

    Fixed matched view

--- app/scripts/matched.js
+++ app/scripts/matched.js
@@ -12,16 +12,23 @@ angular.module('Lunch.matched', ['Lunch.factory.requests'])
       }
   });
 })
-.controller('MatchedCtrl', function($rootScope, $scope, $state, requests, match){
+.controller('MatchedCtrl', function($rootScope, $scope, $state, $window, requests, match){
     var initialize = function() {
       requests.getDetails(match.id).then(function(res) {
-        res.data.photo_url = res.data.profileImage;
-        angular.extend($scope,res.data);
+        var user = {
+          location: res.data.location.id,
+          tags: res.data.tags.map(function(tag) { return tag.name; }),
+          likes: res.data.likes.map(function(like) { return like.name; })
+        };
+
+        angular.extend(res.data.user, user);
+        angular.extend($scope, res.data.user);
       }).catch(function(err) {
         console.error(err);
         $state.go('app.browse');
       });
     };
 
+    $window.localStorage.match = match.id;
     initialize();
 });
