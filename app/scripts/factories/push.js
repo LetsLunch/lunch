5294370bf0e174d167e67a2aa555e42704c9aa31
zzmp <zmp@umich.edu>
2014-06-06 20:19:14 -0700

    Decreased indent in push to standardize/reduce long line

--- app/scripts/factories/push.js
+++ app/scripts/factories/push.js
@@ -7,65 +7,65 @@ angular.module('push', [
   'Lunch.factory.storedUserData',
   'Lunch.factory.requests'
 ])
-  .service('push', function($window, OpenFB, storedUserData, requests, gcmAPI) {
-    var pushNotification;
-    var isMobile;
+.service('push', function($window, OpenFB, storedUserData, requests, gcmAPI) {
+  var pushNotification;
+  var isMobile;
 
-    var register = function() {
-      // Hide pushNotification definition in scope,
-      // as it is undefined until document is 'deviceready'
-      pushNotification = isMobile ?
-        $window.plugins.pushNotification :
-        {
-          register: console.info.bind(console, '(MockGCM) Registered'),
-          unregister: console.info.bind(console, '(MockGCM) Unregistered')
-        };
+  var register = function() {
+    // Hide pushNotification definition in scope,
+    // as it is undefined until document is 'deviceready'
+    pushNotification = isMobile ?
+      $window.plugins.pushNotification :
+      {
+        register: console.info.bind(console, '(MockGCM) Registered'),
+        unregister: console.info.bind(console, '(MockGCM) Unregistered')
+      };
 
-      console.info('Registering with GCM servers');
-      pushNotification.register(
-        function(e) { console.info(e); },
-        function(e) { console.error(e); }, {
-          'senderID': '' + gcmAPI,
-          'ecb': 'onNotificationGCM'
-      });
-    };
-    
-    // Register with the GCM servers
-    this.init = function() {
-      document.addEventListener(
-        'deviceready', function() {
-          isMobile = true;
-          register();
-        }, false);
-    };
-
-    // Register with the application server
-    this.register = function(gcmToken) {
-      // Only reregister if we've registered before
-      if ($window.localStorage.gcmToken && !gcmToken) {
+    console.info('Registering with GCM servers');
+    pushNotification.register(
+      function(e) { console.info(e); },
+      function(e) { console.error(e); }, {
+        'senderID': '' + gcmAPI,
+        'ecb': 'onNotificationGCM'
+    });
+  };
+  
+  // Register with the GCM servers
+  this.init = function() {
+    document.addEventListener(
+      'deviceready', function() {
+        isMobile = true;
         register();
-        // This prevents two registrations on first login
-        return;
-      }
-      $window.localStorage.gcmToken = gcmToken;
-      OpenFB.checkLogin().then(function(fbId) {
-        // Register fb id and push token with application server
-        requests.postPushToken({
-          id: fbId,
-          token: gcmToken,
-          type: 'gcm'
-        });
-      });
-    };
+      }, false);
+  };
 
-    // Unregister from the GCM servers
-    this.unregister = function() {
-      pushNotification.unregister(console.info.bind(console));
-      if ($window.localStorage.gcmToken) {
-        requests.deletePushToken($window.localStorage.gcmToken);
-      }
-    };
-  });
+  // Register with the application server
+  this.register = function(gcmToken) {
+    // Only reregister if we've registered before
+    if ($window.localStorage.gcmToken && !gcmToken) {
+      register();
+      // This prevents two registrations on first login
+      return;
+    }
+    $window.localStorage.gcmToken = gcmToken;
+    OpenFB.checkLogin().then(function(fbId) {
+      // Register fb id and push token with application server
+      requests.postPushToken({
+        id: fbId,
+        token: gcmToken,
+        type: 'gcm'
+      });
+    });
+  };
+
+  // Unregister from the GCM servers
+  this.unregister = function() {
+    pushNotification.unregister(console.info.bind(console));
+    if ($window.localStorage.gcmToken) {
+      requests.deletePushToken($window.localStorage.gcmToken);
+    }
+  };
+});
  
 // ALL GCM notifications come through here. 
 function onNotificationGCM(e) {
